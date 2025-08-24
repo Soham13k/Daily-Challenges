441b361a456fb32d0abab8416d318cc97186628e
Soham K. <161463057+Soham13k@users.noreply.github.com>
2025-08-24 17:33:38 +0530

    Add files via upload

---
+++ task 10.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    unordered_map<string, vector<string>> mp;
+
+    for (string s : strs) {
+        string key = s;
+        sort(key.begin(), key.end());  
+        mp[key].push_back(s);
+    }
+
+    vector<vector<string>> result;
+    for (auto &entry : mp) {
+        result.push_back(entry.second);
+    }
+
+    return result;
+}
+
+int main() {
+    vector<string> strs = {"eat","tea","tan","ate","nat","bat"};
+    
+    vector<vector<string>> groups = groupAnagrams(strs);
+
+    for (auto &group : groups) {
+        cout << "[ ";
+        for (auto &word : group) {
+            cout << word << " ";
+        }
+        cout << "]\n";
+    }
+    return 0;
+}
